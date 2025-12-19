a69a5c1ae891844bb5fc2499939d9c39e762aab0
Alfred Roos <alfred@stensatter.se>
2025-12-19 20:56:30 +0100

    update

--- testgame/src/main/java/com/example/testgame/Main.java
+++ testgame/src/main/java/com/example/testgame/Main.java
@@ -1,24 +1,7 @@
-
 package com.example.testgame;
 
-import java.awt.BorderLayout;
-import java.io.File;
-
 import com.example.speler.Game;
-import com.example.speler.Scene;
-import com.example.speler.swing.JScene;
-import com.example.speler.terminal.TerminalScene;
-import com.example.speler.terminal.TerminalWindow;
 import com.example.speler.swing.JGameWindow;
-import com.example.speler.Vector2;
-import com.example.speler.ecs.systems.ColliderSystem;
-import com.example.speler.resources.ResourceManager;
-import com.example.speler.ecs.ECS;
-import com.example.speler.scripting.GameObject;
-import com.example.speler.scripting.ScriptManager;
-
-import com.example.speler.ecs.components.*;
-import com.example.speler.scripting.GameObject;
 
 
 public class Main {
@@ -26,8 +9,6 @@ public class Main {
 		public static Game game = new Game(new JGameWindow(), new MyScene());
 		
 		public static void main(String[] args) {
-
-				
 				game.run();
     }
 }
